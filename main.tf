a761cac3718d8b3463d5855cdee52f7d5598f117
MekalaSindhu <mekalasaisindhu@gmail.com>
2025-09-30 11:55:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "s3-poc-s3"   # Replace with your S3 bucket name
-    key            = "ec2-s3-poc/terraform.tfstate"  # Path inside the bucket
+    bucket         = "mini-dtf-project-bucket"   # Replace with your S3 bucket name
+    key            = "Mini-dft-project-key/terraform.tfstate"  # Path inside the bucket
     region         = "us-east-1"                      # Your AWS region
       
   }
