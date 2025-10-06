bc7759664c8e4cf15a17818b6e6e3ba3139e8071
Charan Rahul <chrnrhl28@gmail.com>
2025-10-06 13:51:44 +0530

    Update Jenkinsfile for latest pipeline

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "s3-poc-s3"                    # Replace with your S3 bucket name
-    key    = "ec2-s3-poc/terraform.tfstate" # Path inside the bucket
+    bucket = "s3-poc-s3"
+    key    = "ec2-s3-poc/terraform.tfstate"
     region = "us-east-1"
   }
 }
@@ -30,19 +30,4 @@ resource "null_resource" "upload_test_file" {
       "aws s3 cp /home/ec2-user/test_file.txt s3://${aws_s3_bucket.private_bucket.id}/test-files/test_file.txt"
     ]
   }
-}
-
-# -------------------------
-# Terraform outputs
-# -------------------------
-output "private_ec2_private_ip" {
-  value = aws_instance.private_ec2.private_ip
-}
-
-output "public_ec2_public_ip" {
-  value = aws_instance.public_ec2.public_ip
-}
-
-output "s3_bucket_name" {
-  value = aws_s3_bucket.private_bucket.id
 }
