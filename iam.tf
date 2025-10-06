daad4ba411cfe48e4b736066001d92a3abf9a685
Charan Rahul <chrnrhl28@gmail.com>
2025-10-06 13:54:57 +0530

    Update Jenkinsfile for latest pipeline

--- iam.tf
+++ iam.tf
@@ -22,28 +22,28 @@ resource "aws_iam_role" "private_ec2_s3_role" {
 
 # IAM Policy for S3 access (private bucket only)
 resource "aws_iam_policy" "private_s3_policy" {
-  name        = "${var.project_name}-private-s3-policy"
-  description = "Allow private EC2 to access private S3 bucket"
+  name        = "ec2-s3-poc-private-s3-policy"
+  description = "Allow EC2 to read/write to the private S3 bucket"
 
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
-        Effect   = "Allow"
-        Action   = [
+        Effect = "Allow"
+        Action = [
           "s3:GetObject",
           "s3:PutObject",
-          "s3:DeleteObject",
           "s3:ListBucket"
         ]
         Resource = [
-          "arn:aws:s3:::${var.s3_bucket_name}",
-          "arn:aws:s3:::${var.s3_bucket_name}/*"
+          "arn:aws:s3:::${aws_s3_bucket.private_bucket.id}",
+          "arn:aws:s3:::${aws_s3_bucket.private_bucket.id}/*"
         ]
       }
     ]
   })
 }
+
 
 # Attach policy to the private EC2 role
 resource "aws_iam_role_policy_attachment" "private_ec2_s3_attach" {
