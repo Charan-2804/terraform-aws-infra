52115ca6e4d551a6b1a5ae010fc87da3f255a088
MEKALA SAI SINDHU <mekalasaisindhu@gmail.com>
2025-10-07 13:03:16 +0530

    Save work before switching branch

--- main.tf
+++ main.tf
@@ -1,14 +1,12 @@
 terraform {
   backend "s3" {
-    bucket = "s3-poc-s3"
-    key    = "ec2-s3-poc/terraform.tfstate"
-    region = "us-east-1"
+    bucket         = "mini-dtf-project-bucket"          # Your manually created bucket
+    key            = "terraform/state/terraform.tfstate" # Path inside the bucket
+    region         = "us-east-1"                         # Region of your bucket
   }
 }
 
-# -------------------------
 # Null resource to upload test file from private EC2
-# -------------------------
 resource "null_resource" "upload_test_file" {
   depends_on = [
     aws_instance.private_ec2,
@@ -20,14 +18,14 @@ resource "null_resource" "upload_test_file" {
     type         = "ssh"
     host         = aws_instance.private_ec2.private_ip
     user         = "ec2-user"
-    private_key  = file("terraform-poc.pem")       # PEM file in Jenkins workspace
+    private_key  = file("Mini-dft-project-key-pem")
     bastion_host = aws_instance.public_ec2.public_ip
   }
 
   provisioner "remote-exec" {
     inline = [
-      "echo 'This is a test file from private EC2 at $(date)' > /home/ec2-user/test_file.txt",
-      "aws s3 cp /home/ec2-user/test_file.txt s3://${aws_s3_bucket.private_bucket.id}/test-files/test_file.txt"
+      "echo 'This is a test file from private EC2' > /home/ec2-user/test_file.txt",
+      "aws s3 cp /home/ec2-user/test_file.txt s3://${aws_s3_bucket.private_bucket.bucket}/test-files/test_file.txt"
     ]
   }
 }
