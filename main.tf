47f21f0c9d0f994acd88b1d99bdb2a9a5dcd5bc1
MEKALA SAI SINDHU <mekalasaisindhu@gmail.com>
2025-10-13 10:59:52 +0530

    modify

--- main.tf
+++ main.tf
@@ -1,8 +1,7 @@
 terraform {
   backend "s3" {
-
     bucket = "mini-dtf-project-bucket"
-    key    = "Mini-dft-project-key/terraform.tfstate"
+    key    = "s3-poc-s3/terraform.tfstate"
     region = "us-west-1"
   }
 }
